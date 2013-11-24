32f16de9d230f4a5a70f64c96defe1d80083be72
Tomochika Hara <tomochikahara@gmail.com>
2013-11-24 17:06:40 +0900

    fix requires

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -2,9 +2,6 @@ $:.unshift File.dirname(__FILE__)
 
 require "todo_list/version"
 
-require "todo_list/todo.rb"
-require "todo_list/todo_list.rb"
-
 module TodoList
   # Your code goes here...
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require "todo_list"
 
 require 'minitest'
